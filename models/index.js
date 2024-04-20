cdd4abd916765e2f1673485a7d23931b8a7fbd81
jmartincampos <jmartincampos18@gmail.com>
2024-04-20 15:35:09 -0700

    changes

--- models/index.js
+++ models/index.js
@@ -3,13 +3,28 @@ const Category = require('./category');
 const Tag = require('./tag');
 const ProductTag = require('./productTag');
 
-// Products belongsTo Category
+// Products belong to a Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belong to many Tags through ProductTag
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belong to many Products through ProductTag
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
